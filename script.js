bae6170ab829f4580ecaf285700efec05abd0684
ailuropix <purohitpooja321@gmail.com>
2026-05-18 11:59:01 +0530

    thai proxy fee changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let mode = 'thb';
 let baseINR = 0;
 
 // --- CONVERSION FORMULAS ---
-function thbToINR(p) { return ((((p * 600 / 15700 + 0.32) * 4 / 100) + (p * 595 / 15700 + 0.32)) + 0.5) * 100; }
+function thbToINR(p) { return ((((p * 620 / 15700 + 0.32) * 4 / 100) + (p * 595 / 15700 + 0.32)) + 0.5) * 100; }
 function idrToINR(p) { return (((p / 15700 + 0.32) * 4 / 100) + (p / 15700 + 0.32) + 1) * 100; }
 function twdToINR(p) { return (p * 4) + 100; } 
 function cnyToINR(p) { return (p * 17) + 100; } // UPDATED: price * 17 + 100
